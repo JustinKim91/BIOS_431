3424c5db8a6d3be710daae5a85b53fdd32b83c97
Justin Kim <jk91@rice.edu>
2024-09-05 18:17:54 -0500

    Lab 1

--- BIOS_431_Lab_1.R
+++ BIOS_431_Lab_1.R
@@ -60,7 +60,48 @@ x
 
 ## Visualizing, analyzing, and exporting data (4 points) ----
 
+### 1 ----
 # The histogram of x displays a relatively uniform bell-shaped curve
 hist(x)
+
+### 2 ----
+
+y_true <- (2*x) + 10
+
+### 3 ----
+
+y_obs <- rnorm(1000, mean = y_true, sd = 2)
+
+### 4 ----
+# library(ggplot2)
+
+severity <- data.frame(x, y_obs)
+
+severity_plot <- ggplot(severity, aes(x = x, y = y_obs)) + 
+  geom_point() +
+  xlab("Trait") +
+  ylab("Disease Severity")
+
+severity_plot
+
+### 5 ----
+
+severity_plot_lr <- severity_plot + geom_smooth(method = "lm", formula = y ~ x)
+model_1 <- lm(y_obs ~ x)
+summary(model_1)
+
+# The summary yields an estimate for the relationship as y_obs = 2.036x + 9.951
+# ... , which is pretty similar to the perfect model of 2x + 10.Furthermore, the
+# ... standard error for these coefficients are around 0.06, while the p-values
+# ... are highly significant. Thus, this regression correctly estimates the
+# ... relationship between the trait and the disease severity.
+
+### 6 ----
+
+# I previously already completed this step, in order to create a scatter plot
+# ... using ggplot2, which I named "severity"
+disease.data <- data.frame(x, y_obs)
+
+
 
 
