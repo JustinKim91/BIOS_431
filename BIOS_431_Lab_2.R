db7430ef15505a9542bdf048398e855b59bc6ea1
Justin Kim <justinpeterkim@gmail.com>
2024-09-13 12:03:22 -0500

    Lab 2

--- BIOS_431_Lab_2.R
+++ BIOS_431_Lab_2.R
@@ -2,7 +2,6 @@
 # Justin Kim
 # 09/13/2024
 
-library(deSolve)
 
 ## Simulating a model in R ----
 
@@ -19,3 +18,45 @@ exponential_growth = function (t, y, parameters){
     }
   )
 }
+
+# Load package
+library(deSolve)
+
+# Parameters
+
+initials = c(N=10)
+params = c(b = 0.3, d = 0.1)
+t = 0:100
+
+# Simulation
+
+exp_grow_sim = lsoda(y = initials, times = t, parms = params, func = exponential_growth)
+
+exp_grow_sim
+plot(exp_grow_sim)
+# abline(h = 2000000000)
+
+# Model Check
+
+N0 = 10
+b = 0.3
+d = 0.1
+curve(N0*exp((b-d)*x), from = 0, to = 100, col = 'red', add = T)
+
+## Question 1 ----
+
+# Population that grows indefinitely
+initials = c(N=10)
+params = c(b = 0.3, d = 0.1)
+t = 0:100
+
+plot(exp_grow_sim)
+
+
+# Population that crashes
+initials = c(N=10)
+params_crash = c(b = 0.3, d = 0.4)
+t = 0:100
+
+crash_sim = lsoda(y = initials, times = t, parms = params_crash, func = exponential_growth)
+plot(crash_sim)
