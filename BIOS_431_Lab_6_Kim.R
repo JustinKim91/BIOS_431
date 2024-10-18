b53c8403177e80aac100c70e57d8601a30bfe15e
Justin Kim <jk91@rice.edu>
2024-10-18 05:32:59 -0500

    Lab 6

---
+++ BIOS_431_Lab_6_Kim.R
@@ -0,0 +1,10 @@
+# BIOS 431 Emerging Infectious Diseases Lab 6 ----
+# Justin Kim
+# 10/18/2024
+
+## Question 1 -----------------------------------------------------------------
+library(network)
+
+# Loading badgers.csv data
+
+badger <- read.csv("../badgers.csv")
