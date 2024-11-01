09f435c0abed1f341a23c5cf6d3ab7dc9d87705b
Justin Kim <jk91@rice.edu>
2024-11-01 09:54:16 -0500

    Lab 7

--- BIOS_431_Lab_7_Kim.R
+++ BIOS_431_Lab_7_Kim.R
@@ -36,6 +36,7 @@ MultiSI<-function (t, y, parameters){
 
 
 ## Question 2 -----------------------------------------------------------------
+library(ggplot2)
 
 params = c(b = 0.2, d = 0.05, K= 150, beta11 = 0.0001, beta22 = 0.001, beta12=0.000,
            beta21=0.000, v = 0.001)
@@ -44,6 +45,90 @@ timespan=500 #duration of the simulation
 t = seq(0,timespan,0.1) #created a sequence of time steps
 steps=length(t) #we keep track of time steps in case we want to change the duration
 
-mh_SI <- lsoda(y = initial, times = t, parms = params, func = MultiSI)
+mh_SI <- data.frame(lsoda(y = initial, times = t, parms = params, func = MultiSI))
 mh_SI
+
+
+Q2_plot <- ggplot(mh_SI, aes(x = time)) +
+  geom_line(aes(y = S1, colour = 'Species 1 Susceptible')) +
+  geom_line(aes(y = I1, colour = 'Species 1 Infected')) +
+  geom_line(aes(y = S2, colour = 'Species 2 Susceptible')) +
+  geom_line(aes(y = I2, colour = 'Species 2 Infected')) +
+  xlab("Time") +
+  ylab("Population") +
+  ggtitle("Multi-Host SI-Model") +
+  scale_colour_manual(name = "Legend",
+                      values = c('Species 1 Susceptible' = 'dodgerblue', 
+                                 'Species 1 Infected' = 'red', 
+                                 'Species 2 Susceptible' = 'darkgreen', 
+                                 'Species 2 Infected' = 'purple')) +
+  theme(legend.position = c(0.85, 0.75))
+
+Q2_plot
+
+
+### a -----
+
+# Based on the results of the simulation, I would predict that species 2 poses
+# ... a greater risk of spillover than species 1. This is because, in the 
+# ... species 1 population, there is a very low prevalence of the infected
+# ... population, whereas the infected population grows to a very large proportion
+# ... of the species 2 population. This indicates that the population of species
+# ... sustains a greater level of infected population, making it more likely for
+# ... a cross-species infection to go from species 2 to species 1.
+
+### b ----
+
+# The conditions in which the transmission from species 1 to 2 is nonzero
+params_12 = c(b = 0.2, d = 0.05, K= 150, beta11 = 0.0001, beta22 = 0.001, beta12=0.001,
+            beta21=0.000, v = 0.001)
+
+test_12 = data.frame(lsoda(y = initial, times = t, parms = params_12, func = MultiSI))
+
+
+# The conditions in which the transmission from species 2 to 1 is nonzero
+params_21 = c(b = 0.2, d = 0.05, K= 150, beta11 = 0.0001, beta22 = 0.001, beta12=0.000,
+              beta21=0.001, v = 0.001)
+
+test_21 = data.frame(lsoda(y = initial, times = t, parms = params_21, func = MultiSI))
+
+# Plot for when the transmission from species 1 to 2 is nonzero
+
+test_plot1 <- ggplot(test_12, aes(x = time)) +
+  geom_line(aes(y = S1, colour = 'Species 1 Susceptible')) +
+  geom_line(aes(y = I1, colour = 'Species 1 Infected')) +
+  geom_line(aes(y = S2, colour = 'Species 2 Susceptible')) +
+  geom_line(aes(y = I2, colour = 'Species 2 Infected')) +
+  xlab("Time") +
+  ylab("Population") +
+  ggtitle("Multi-Host SI-Model") +
+  scale_colour_manual(name = "Legend",
+                      values = c('Species 1 Susceptible' = 'dodgerblue', 
+                                 'Species 1 Infected' = 'red', 
+                                 'Species 2 Susceptible' = 'darkgreen', 
+                                 'Species 2 Infected' = 'purple')) +
+  theme(legend.position = c(0.85, 0.75))
+
+test_plot1
+
+# Plot for when the transmission from species 2 to 1 is nonzero
+
+test_plot2 <- ggplot(test_21, aes(x = time)) +
+  geom_line(aes(y = S1, colour = 'Species 1 Susceptible')) +
+  geom_line(aes(y = I1, colour = 'Species 1 Infected')) +
+  geom_line(aes(y = S2, colour = 'Species 2 Susceptible')) +
+  geom_line(aes(y = I2, colour = 'Species 2 Infected')) +
+  xlab("Time") +
+  ylab("Population") +
+  ggtitle("Multi-Host SI-Model") +
+  scale_colour_manual(name = "Legend",
+                      values = c('Species 1 Susceptible' = 'dodgerblue', 
+                                 'Species 1 Infected' = 'red', 
+                                 'Species 2 Susceptible' = 'darkgreen', 
+                                 'Species 2 Infected' = 'purple')) +
+  theme(legend.position = c(0.85, 0.75))
+
+test_plot2
+
+
 
