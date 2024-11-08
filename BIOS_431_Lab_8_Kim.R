e77831ea1c92c0af1c498a68ec11bb1950d52cf4
Justin Kim <jk91@rice.edu>
2024-11-08 03:01:53 -0600

    Lab 8

---
+++ BIOS_431_Lab_8_Kim.R
@@ -0,0 +1,79 @@
+# BIOS 431 Emerging Infectious Diseases Lab 8 ----
+# Justin Kim
+# 11/08/2024
+
+## Question 1 -----------------------------------------------------------------
+
+# Our group predicted that the hosts would win, given that there would be a
+# ... constant re-flux of random suits (genes) in the host population when a
+# ... large portion of the population succumbs to the disease. In contrast,
+# ... when a parasite survives, it more heavily invests in the surviving gene
+# ... (1 card for host reproduction vs 2 cards for parasite reproduction).
+
+## Question 2 -----------------------------------------------------------------
+
+# Neither the host nor the parasite really "won" the game, as both populations
+# ... persisted and went through upheavals of genetic composition.However, in
+# ... a sense, we can argue that the host "won," given that the host population
+# ... persisted despite the parasite induced deaths. This is determination of
+# ... the winner is based on the perspective that the goal of the host is to
+# ... survive while the goal of the parasite is to kill. 
+
+## Question 3 -----------------------------------------------------------------
+
+# This did not entirely meet our initial prediction that the host population
+# ... would be victorious. Our prediction was that the higher reproductive 
+# ... investment of parasites, in comparison to the hosts would cause an
+# ... evolution of the host population to eradicate the parasites. However, we
+# ... observed a back-and-forth shift of genotypes between the two populations.
+
+## Question 4 -----------------------------------------------------------------
+
+# For the hosts, the Red Queen Hypothesis prediction of genetic variation
+# ... during host-parasite coevolution seemed to hold true for the most part.
+# ... While there were simulation that ended up with a skew towards specific
+# ... genotypes, there were definitely observable variations throughout the
+# ... game for the host population.
+
+# The same can be said for the parasite population. Both host and parasite
+# ... populations experienced loss and maintenance of genetic variation. The
+# ... genetic variation of either population heavily depended upon the
+# ... host-parasite interactions during the game: natural selection
+# ... (non-matching cards) caused decrease in variation in both populations as
+# ... reproduction selected for the genotype that survived most. Mutations
+# ... (selection of random cards to fill 12) created variation by adding new
+# ... and random genotypes.
+
+## Question 5 -----------------------------------------------------------------
+
+# If there was an absence of parasites, I would predict that the genotype
+# ... frequencies would remain some what stable. To test these conditions,
+# ... I would simulate the game with the same rules, but just have no parasite
+# ... population. Thus, every host would survive (not die of parasite related
+# ... health complications). In this model, each surviving member would produce
+# ... a single offspring of the same genotype, essentially doubling the
+# ... population, but maintaining the genotype proportions before selection of 
+# ... of the new generation. Under these circumstances, I would reason that the
+# ... genetic composition of the host population would remain relatively stable
+
+## Question 6 -----------------------------------------------------------------
+
+# The oscillations for host and parasite genotypes are not perfectly synced.
+# ... The host genotype oscillation generally lags behind that of the host
+# ... population by about 1-2 generations. This is likely due to the parasite
+# ... evolution in response to the host genotype frequencies. During the game,
+# ... we noticed that, given a high frequency of a genotype in a host, the
+# ... parasite had a strong chance of surviving a generation since there was a
+# ... high likelihood of matching the parasite genotype to the high-frequency
+# ... genotype of the host. In turn, the parasite reproduced heavily towards
+# ... this specific genotype (2 cards) and began to increase in this genotype.
+# ... This would then cause a decrease in the host genotype, as more matches
+# ... showed up, and the cycle continued this way.Thus, both oscillations
+# ... technically lag behind each other, but in the context of the game, I
+# ... think that it would be more appropriate to detail the parasite population
+# ... lagging behind the host.
+
+
+
+
+
