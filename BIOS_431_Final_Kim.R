5171aebb8df1d055e769925589c6d5480e483e0b
Justin Kim <justinpeterkim@gmail.com>
2024-12-01 12:31:46 -0600

    Final

--- BIOS_431_Final_Kim.R
+++ BIOS_431_Final_Kim.R
@@ -2,8 +2,8 @@
 
 
 
-
-
+library(deSolve)
+library(ggplot2)
 
 
 
@@ -16,7 +16,7 @@ SIR = function (t, state, parameters){
       
       dSdt = (b*(1 - c*(S + I + R))) * (S + I + R) - (d + k + r)*S - B*S*I + w*R
       dIdt = B*S*I - (d + v + k)*I
-      dRdt = r*S - (w + d + k)*R
+      dRdt = r_adj*S - (w + d + k)*R
       res = c(dSdt, dIdt, dRdt)
       list(res)
     }
@@ -27,7 +27,55 @@ SIR = function (t, state, parameters){
 # Parameters
 initials = c(S = 99, I = 1, R = 0)
 params1 = c(b = 0.5, d = 0.05, c = 0.01, B = 0.01, 
-            w = 0.05, v = 0.15, k = 0, r =0, f = 0)
+            w = 0.05, v = 0.1, k = 0, r = 0.5, f = 0)
 t = 0:100
 
+# Sequencing a fragmentation vector from o to 1 in increments of 0.01
+
+frag_vec <- seq(0,1,0.01)
+
+# Creating empty vectors for S, R, and I values for the end of simulation
+
+Susceptible = numeric()
+Infected = numeric()
+Recovered = numeric()
+
+# For loop iterating across f
+for (i in 1:length(frag_vec)){
+  params1["f"] = frag_vec[i]
+  output = lsoda(y = initials, times = t, 
+                 func = SIR, parms = params1)
+  
+  Susceptible[i] = output[101, 2]
+  Infected[i] = output[101, 3]
+  Recovered[i] = output[101, 4]
+  
+}
+
+# SIR data
+data1 = lsoda(y = initials, times = t, 
+              func = SIR, parms = params1)
+
+
+# Creating a data frame to store the densities and frag values
+results = data.frame(frag_vec, Susceptible, Infected, Recovered)
+results
+
+
+amph_plot <- ggplot(results, aes(x = frag_vec)) +
+  geom_line(aes(y = Susceptible), color = "darkgreen") +
+  geom_line(aes(y = Infected), color = "red") +
+  geom_line(aes(y = Recovered), color = "purple") +
+  ggtitle("SIR Population vs Fragmentation") +
+  xlab("Degree of Fragmentation") +
+  ylab("Individuals")
+
+amph_plot  
+  
+  
+  
+  
+  
+  
+  
 
---
+++ final_testing.R
@@ -0,0 +1,162 @@
+SIR = function (t, state, parameters){
+  with(
+    as.list(c(state, parameters)),
+    {
+      # Total population
+      N = S + I + R
+      
+      # Adjusted recovery rate with fragmentation
+      r_adj <- r * (1 - f)
+      
+      # Birth rate that depends on current population
+      birth_term = b * (1 - N / K)
+      
+      dSdt = birth_term * N - (d + k + r_adj)*S - B*S*I/N + w*R
+      dIdt = B*S*I/N - (d + v + k)*I
+      dRdt = r_adj*S - (w + d + k)*R
+      
+      res = c(dSdt, dIdt, dRdt)
+      list(res)
+    }
+  )
+}
+
+
+params1 = c(
+  b = 0.5,        # Increased birth rate
+  d = 0.01,       # Low death rate
+  K = 10000,      # Carrying capacity set to initial population
+  c = 0.0001,     # Very small carrying capacity effect
+  B = 0.0001,     # Much lower transmission rate
+  w = 0.01,       # Low rate of losing immunity
+  v = 0.05,       # Recovery rate
+  k = 0.001,      # Very low additional removal rate
+  r = 0.05,       # Moderate recovery/vaccination rate
+  f = 0           # Fragmentation starts at 0
+)
+
+
+initials = c(S = 8000, I = 2000, R = 0)
+
+# Storage for results
+results <- data.frame(
+  Fragmentation = frag_vec,
+  Susceptible = numeric(length(frag_vec)),
+  Infected = numeric(length(frag_vec)),
+  Recovered = numeric(length(frag_vec)),
+  TotalPopulation = numeric(length(frag_vec))
+)
+
+# Simulation loop
+for (i in 1:length(frag_vec)){
+  params1["f"] = frag_vec[i]
+  
+  output = lsoda(
+    y = initials, 
+    times = seq(0, 100, by = 1), 
+    func = SIR, 
+    parms = params1
+  )
+  
+  
+  # Store final time point values
+  results$Susceptible[i] = output[nrow(output), 2]
+  results$Infected[i] = output[nrow(output), 3]
+  results$Recovered[i] = output[nrow(output), 4]
+  results$TotalPopulation[i] = sum(output[nrow(output), 2:4])
+}
+
+# Plotting
+par(mfrow=c(2,1))
+plot(results$Fragmentation, results$Susceptible, type = 'l', col = 'blue', 
+     main = "Population Compartments vs Fragmentation",
+     xlab = "Fragmentation", ylab = "Population")
+lines(results$Fragmentation, results$Infected, col = 'red')
+lines(results$Fragmentation, results$Recovered, col = 'green')
+legend("topright", 
+       legend = c("Susceptible", "Infected", "Recovered"),
+       col = c("blue", "red", "green"),
+       lty = 1)
+
+plot(results$Fragmentation, results$TotalPopulation, type = 'l',
+     main = "Total Population vs Fragmentation",
+     xlab = "Fragmentation", ylab = "Total Population")
+
+
+# Prettier Plots
+
+pretty_plot <- ggplot(results, aes(x = Fragmentation)) +
+  geom_line(aes(y = Susceptible), color = 'green') +
+  geom_line(aes(y = Infected), color = 'red') +
+  geom_line(aes(y = Recovered), color = 'purple') +
+  xlab("Fragmentation") +
+  ylab("Individuals")
+
+pretty_plot
+
+
+
+
+
+# More Testing
+
+initials = c(S = 8000, I = 2000, R = 0)
+
+params1 = c(
+  b = 0.2,        # Increased birth rate
+  d = 0.01,       # Low death rate
+  K = 10000,      # Carrying capacity set to initial population
+  c = 0.0001,     # Very small carrying capacity effect
+  B = 0.0001,     # Much lower transmission rate
+  w = 0.01,       # Low rate of losing immunity
+  v = 0.05,       # Recovery rate
+  k = 0.001,      # Very low additional removal rate
+  r = 0.5,       # Moderate recovery/vaccination rate
+  f = 0           # Fragmentation starts at 0
+)
+
+test = lsoda(y = initials, times = seq(0, 100, by = 1), parms = params1, func = SIR)
+
+test1_plot <- ggplot(test, aes(x = time)) +
+  geom_line(aes(y = S), color = 'dodgerblue') + 
+  geom_line(aes(y = I), color = 'darkred') +
+  geom_line(aes(y = R), colour = 'purple') +
+  xlab("Time") +
+  ylab("Number of Individuals") +
+  ggtitle("Disease Dynamic for No Fragmentation")
+
+test1_plot
+
+
+
+
+
+params2 = c(
+  b = 0.2,        # Increased birth rate
+  d = 0.01,       # Low death rate
+  K = 10000,      # Carrying capacity set to initial population
+  c = 0.0001,     # Very small carrying capacity effect
+  B = 0.0001,     # Much lower transmission rate
+  w = 0.01,       # Low rate of losing immunity
+  v = 0.05,       # Recovery rate
+  k = 0.001,      # Very low additional removal rate
+  r = 0.5,       # Moderate recovery/vaccination rate
+  f = 1           # Fragmentation starts at 0
+)
+
+
+test2 = lsoda(y = initials, times = seq(0, 100, by = 1), parms = params2, func = SIR)
+
+test2_plot <- ggplot(test2, aes(x = time)) +
+  geom_line(aes(y = S), color = 'dodgerblue') + 
+  geom_line(aes(y = I), color = 'darkred') +
+  geom_line(aes(y = R), colour = 'purple') +
+  xlab("Time") +
+  ylab("Number of Individuals") +
+  ggtitle("Disease Dynamic for Fragmentation")
+
+
+test2_plot
+
+
+
